3fcc0a4e9232a384ee72464963680ffbf1cc2cad
wky <wangkaiyu1103@gmail.com>
2023-03-04 23:48:41 +0800

    add request date log

--- service/src/index.ts
+++ service/src/index.ts
@@ -34,7 +34,8 @@ router.post('/chat-process', async (req, res) => {
     let firstChunk = true
     await chatReplyProcess(prompt, options, (chat: ChatMessage) => {
       res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
-      global.console.log()
+      global.console.log(prompt)
+      global.console.log(new Date())
       firstChunk = false
     })
   }
